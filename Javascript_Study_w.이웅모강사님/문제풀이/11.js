f8ef7f0a402d2f8396346166dda7504243a9037b
JIHU96 <korjihu@gmail.com>
2024-04-04 21:08:54 +0900

    upload: 11, 12번 문제 풀이

--- Javascript_Study_w.이웅모강사님/문제풀이/11.js
+++ Javascript_Study_w.이웅모강사님/문제풀이/11.js
@@ -1,24 +1,18 @@
-/* 
-
+/* 더 크게 합치기
+연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환
+ 12 ⊕ 3 = 123
+ 3 ⊕ 12 = 312
+a ⊕ b와 b ⊕ a 중 더 큰 값을 return 
+a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 
 */
 
-const solution1 = () => {
-  //
-  return;
+const solution = (a, b) => {
+  // 연산 함수 정의
+  const concat = (num1, num2) => Number(`${num1}${num2}`);
+
+  return concat(a, b) >= concat(b, a) ? concat(a, b) : concat(b, a);
 };
 
-const solution2 = () => {
-  //
-  return;
-};
-
-const solution3 = () => {
-  //
-  return;
-};
-
-console.log("===solution1===");
-
-console.log("===solution2===");
-
-console.log("===solution3===");
+console.log("===solution===");
+console.log(solution(9, 91)); // 991
+console.log(solution(89, 8)); // 898
--- Javascript_Study_w.이웅모강사님/문제풀이/12.js
+++ Javascript_Study_w.이웅모강사님/문제풀이/12.js
@@ -1,24 +1,21 @@
-/* 
-
+/* 두 수의 연산값 비교하기
+연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환
+ 12 ⊕ 3 = 123
+ 3 ⊕ 12 = 312
+a ⊕ b와 2 * a * b 중 더 큰 값을 return
+a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 
 */
 
-const solution1 = () => {
-  //
-  return;
+const solution = (a, b) => {
+  // 연산 함수 정의
+  const concat = (num1, num2) => Number(`${num1}${num2}`);
+  const multiple2_A_B = (num1, num2) => 2 * num1 * num2;
+
+  return concat(a, b) >= multiple2_A_B(a, b)
+    ? concat(a, b)
+    : multiple2_A_B(a, b);
 };
 
-const solution2 = () => {
-  //
-  return;
-};
-
-const solution3 = () => {
-  //
-  return;
-};
-
-console.log("===solution1===");
-
-console.log("===solution2===");
-
-console.log("===solution3===");
+console.log("===solution===");
+console.log(solution(2, 91)); // 364
+console.log(solution(91, 2)); // 912
