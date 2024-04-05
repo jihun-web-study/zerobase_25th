6161dacedee707d56cdfb0711ab4518bae09158e
JIHU96 <korjihu@gmail.com>
2024-04-05 09:20:59 +0900

    upload: 16~30번 껍데기파일 추가

---
+++ Javascript_Study_w.이웅모강사님/문제풀이/30.js
@@ -0,0 +1,24 @@
+/* 
+
+*/
+
+const solution1 = () => {
+  //
+  return;
+};
+
+const solution2 = () => {
+  //
+  return;
+};
+
+const solution3 = () => {
+  //
+  return;
+};
+
+console.log("===solution1===");
+
+console.log("===solution2===");
+
+console.log("===solution3===");
