23bedc166a7428181a67def8def0de8bee8d41d8
JIHU96 <korjihu@gmail.com>
2024-04-12 15:27:59 +0900

    upload: 61~75번 문제 껍데기 파일 추가

---
+++ Javascript_Study_w.이웅모강사님/문제풀이/61~75/75.js
@@ -0,0 +1,26 @@
+/* 
+
+*/
+
+const solution1 = () => {
+  //
+  return;
+};
+
+const solution2 = () => {
+  //
+  return;
+};
+
+const solution3 = () => {
+  //
+  return;
+};
+
+console.log("===solution1===");
+
+console.log("===solution2===");
+
+console.log("===solution3===");
+
+/* == 라이브 도중 추가 == */
