e4b1d81bfd49d5a5c6770bb7513bbd74c98a551d
JIHU96 <korjihu@gmail.com>
2024-04-25 15:20:26 +0900

    fix: 기존 15개마다 분할한걸 10개씩으로 변경

--- Javascript_Study_w.이웅모강사님/문제풀이/91~100/91.js
+++ Javascript_Study_w.이웅모강사님/문제풀이/91~100/91.js
@@ -1,26 +1,16 @@
-/* 
+/* 진료 순서 정하기
+환자의 응급도를 기준으로 진료 순서
 
 */
 
-const solution1 = () => {
-  //
-  return;
+const solution = (emergency) => {
+  const map = new Map(emergency, 0);
+
+  return map;
 };
 
-const solution2 = () => {
-  //
-  return;
-};
-
-const solution3 = () => {
-  //
-  return;
-};
-
-console.log("===solution1===");
-
-console.log("===solution2===");
-
-console.log("===solution3===");
+console.log(solution([3, 76, 24])); // [3, 1, 2]
+console.log(solution([1, 2, 3, 4, 5, 6, 7])); // [7, 6, 5, 4, 3, 2, 1]
+console.log(solution([30, 10, 23, 6, 100])); // [2, 4, 3, 5, 1]
 
 /* == 라이브 도중 추가 == */
