50b483018a2d685cd683e3625b48b803bf89647b
JIHU96 <korjihu@gmail.com>
2024-06-15 23:15:19 +0900

    study: 이진탐색트리 삭제 구현

--- study/DataStructure/tree/binary_tree/binary_search_tree.js
+++ study/DataStructure/tree/binary_tree/binary_search_tree.js
@@ -38,6 +38,22 @@ class BinarySearchTree {
     console.log(`end\n`);
   }
 
+  // 노드 최소값 탐색
+  #minNode(node) {
+    let currentNode = node;
+
+    if (currentNode === null) return null;
+    while (currentNode && currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+
+    return currentNode.value;
+  }
+
+  min() {
+    return this.#minNode(this.root);
+  }
+
   // 노드 최대값 탐색
   #maxNode(node) {
     if (node === null) return null;
@@ -49,39 +65,53 @@ class BinarySearchTree {
   }
 
   max() {
-    this.root = this.#maxNode(this.root);
-  }
-
-  // 노드 최소값 탐색
-  #minNode(node) {
-    if (node === null) return null;
-    while (node && node.left !== null) {
-      node = node.left;
-    }
-
-    return node.value;
-  }
-
-  min() {
-    this.root = this.#minNode(this.root);
+    return this.#maxNode(this.root);
   }
 
   // 노드 특정값 탐색
   #searchNode(node, value) {
-    return node;
+    if (node === null) return false;
+
+    if (node.value === value) return true;
+    else if (node.value > value) return this.#searchNode(node.left, value);
+    else if (node.value < value) return this.#searchNode(node.right, value);
   }
 
   search(value) {
-    this.root = this.#searchNode(this.root, value);
+    return this.#searchNode(this.root, value);
+  }
+
+  // 반복문으로 트리를 순회하여 최소값을 보유한 노드 탐색
+  #findMinNode(node) {
+    while (node && node.left !== null) {
+      node = node.left;
+    }
+    return node;
   }
 
   // 노드 삭제
   #removeNode(node, value) {
+    if (node === null) return null;
+    if (node.value === value) {
+      // case 1: leaf node
+      if (node.left === null && node.right === null) node = null;
+      // case 2: 1 child node
+      else if (node.left === null) node = node.right;
+      else if (node.right === null) node = node.left;
+      // case 3: 2 child node
+      else {
+        let aux = this.#findMinNode(node.right);
+        node.value = aux.value;
+        node.right = this.#removeNode(node.right, aux.value);
+      }
+    } else if (node.value > value) node.left = this.#removeNode(node.left, value);
+    else if (node.value < value) node.right = this.#removeNode(node.right, value);
+
     return node;
   }
 
   remove(value) {
-    this.root = this.#removeNode(this.root, value);
+    return this.#removeNode(this.root, value);
   }
 }
 
@@ -109,3 +139,20 @@ tree.inOrderTraverse(printNode);
 console.log(`==== Min & Max ====`);
 console.log("min: ", tree.min());
 console.log("max: ", tree.max());
+
+console.log(`\n==== Search ====`);
+console.log("Has A?: ", tree.search("A"));
+console.log("Has I?: ", tree.search("I"));
+console.log("Has J?: ", tree.search("J"));
+
+console.log(`\n==== Remove ====`);
+
+tree.inOrderTraverse(printNode);
+tree.remove("H");
+tree.inOrderTraverse(printNode);
+tree.remove("D");
+tree.inOrderTraverse(printNode);
+tree.remove("F");
+tree.inOrderTraverse(printNode);
+
+console.log(tree.root);
