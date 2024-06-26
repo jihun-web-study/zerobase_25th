f9f4017cab9bdcbbff7eb1dad93cc048c156a81e
JIHU96 <korjihu@gmail.com>
2024-06-26 19:00:29 +0900

    study: react.memo 학습

--- react-study/src/pages/C/C.jsx
+++ react-study/src/pages/C/C.jsx
@@ -1,10 +1,28 @@
+import React, { memo, useEffect, useState, useCallback } from "react";
+
 export default function C() {
+  const [value, setValue] = useState(0);
+
+  const incrementValue = useCallback(() => {
+    setValue((c) => c + 1);
+  }, []);
+
+  console.log("Parent render");
+
   return (
     <div>
       <p>C</p>
       <form action="/" method="get">
         <input type="number" placeholder={0} inputMode="numeric" pattern="[0-9]*" />
       </form>
+      <button onClick={incrementValue}>click</button>
+      <Child value={value} />
     </div>
   );
 }
+
+const Child = React.memo((props) => {
+  console.log("child", props.value);
+
+  return <div>child</div>;
+});
